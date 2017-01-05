e114b9b67999380ee4e69725077175019747411c
psycura <psycura@gmail.com>
2017-01-05 20:42:01 +0200

    Create base structure

---
+++ src/index.js
@@ -0,0 +1,13 @@
+let str = require('./constant');
+let $ = require('jquery');
+
+let App = function() {
+    // console.log(str);
+};
+
+let app = new App();
+app.get = function() {
+
+};
+
+$('body').html(str);
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+let webpack = require('webpack');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/public',
+        filename: 'bundle.js'
+    }
+};
