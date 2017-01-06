1fb12e1e9594bb9d6c97ca67c759cd7a45f985d2
psycura <psycura@gmail.com>
2017-01-06 15:02:41 +0200

    Create base structure

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,14 @@ module.exports = {
     output: {
         path: __dirname + '/public',
         filename: 'bundle.js'
+    },
+    module:{
+        loaders:[
+            {
+                // test:/\.scss$/,
+                // loader:'style!css!scss'
+            }
+        ]
     }
+    
 };
